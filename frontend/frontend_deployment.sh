9350a807cee264f04f39cc704ca345a316eceb6e
Venugopal Ibharampur <ibharampur@Venugopals-MacBook-Pro.local>
2025-10-24 21:32:32 +0530

    Fixed platform background colour

--- frontend/frontend_deployment.sh
+++ frontend/frontend_deployment.sh
@@ -5,11 +5,11 @@
 
 set -e  # Exit on any error
 
-echo "⚙️  Configuring AWS credentials..."
-aws configure set aws_access_key_id AKIASTSO7DEBOUDYFFBI
-aws configure set aws_secret_access_key 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
-aws configure set default.region ap-south-1
-aws configure set default.output json
+#echo "⚙️  Configuring AWS credentials..."
+#aws configure set aws_access_key_id AKIASTSO7DEBOUDYFFBI
+#aws configure set aws_secret_access_key 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
+#aws configure set default.region ap-south-1
+#aws configure set default.output json
 
 echo "🔐 Logging into AWS ECR..."
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 179480500482.dkr.ecr.ap-south-1.amazonaws.com
